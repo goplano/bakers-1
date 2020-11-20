69832c1cecad40bf4a96c9f7213b8c7d7fa27122
Sue Carson <suecarson@gmail.com>
2020-11-20 08:59:47 -0600

    need to fix webpack for deploy

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    watch: true,
+    watch: false,
     entry: './src/index.js',
     module: {
         rules: [{
@@ -48,11 +48,4 @@ module.exports = {
             defaultAttribute: 'async'
         })
     ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        index: 'index.html', liveReload: true,
-        hotOnly: true,
-        compress: true,
-        port: 8000
-    }
 };
